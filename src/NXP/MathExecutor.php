5eb4ff5fa7e093519489b27b58e0e5047f1bc6df
zhangkun <zhangkun@mafengwo.com>
2018-05-22 14:52:57 +0800

    对逗号处理的bug修复,增加对中间结果表达式的支持,增加自定义函数

--- src/NXP/MathExecutor.php
+++ src/NXP/MathExecutor.php
@@ -30,6 +30,13 @@ class MathExecutor
     private $variables = array();
 
     /**
+     * middle results
+     *
+     * @var array
+     */
+    private $midresults = array();
+
+    /**
      * @var TokenFactory
      */
     private $tokenFactory;
@@ -45,11 +52,13 @@ class MathExecutor
     public function __construct()
     {
         $this->addDefaults();
+        $this->addCustomFunc();
     }
 
     public function __clone()
     {
         $this->addDefaults();
+        $this->addCustomFunc();
     }
 
     /**
@@ -73,12 +82,69 @@ class MathExecutor
         $this->tokenFactory->addFunction('atn', 'atan');
         $this->tokenFactory->addFunction('min', 'min', 2);
         $this->tokenFactory->addFunction('max', 'max', 2);
+        $this->tokenFactory->addFunction('log10', 'log10', 1);
+        $this->tokenFactory->addFunction('sqrt', 'sqrt', 1);
         $this->tokenFactory->addFunction('avg', function($arg1, $arg2) { return ($arg1 + $arg2) / 2; }, 2);
 
         $this->setVars(array(
             'pi' => 3.14159265359,
             'e'  => 2.71828182846
         ));
+    }
+
+    /**
+     * Set custom functions
+     */
+    protected function addCustomFunc() {
+        $this->tokenFactory->addFunction('log', function ($arg1, $arg2) { return log($arg2, $arg1); }, 2);
+        $this->tokenFactory->addFunction('gt', function ($arg1, $arg2, $arg3, $arg4) {
+            return $arg4 > $arg3 ? $arg2 : $arg1;
+        }, 4);
+        $this->tokenFactory->addFunction('egt', function ($arg1, $arg2, $arg3, $arg4) {
+            return $arg4 >= $arg3 ? $arg2 : $arg1;
+        }, 4);
+        $this->tokenFactory->addFunction('lt', function ($arg1, $arg2, $arg3, $arg4) {
+            return $arg4 < $arg3 ? $arg2 : $arg1;
+        }, 4);
+        $this->tokenFactory->addFunction('elt', function ($arg1, $arg2, $arg3, $arg4) {
+            return $arg4 <= $arg3 ? $arg2 : $arg1;
+        }, 4);
+        $this->addComplexFunc();
+    }
+
+    /**
+     * Set complex custom functions
+     */
+    protected function addComplexFunc() {
+        $this->tokenFactory->addFunction('lteach', function ($arg1, $arg2, $arg3, $arg4, $arg5, $arg6) {
+            if($arg6 < $arg5) {
+                return $arg4;
+            } elseif($arg6 < $arg3) {
+                return $arg2;
+            } else {
+                return $arg1;
+            }
+        }, 6);
+        $this->tokenFactory->addFunction('elteach', function ($arg1, $arg2, $arg3, $arg4, $arg5, $arg6) {
+            if($arg6 <= $arg5) {
+                return $arg4;
+            } elseif($arg6 <= $arg3) {
+                return $arg2;
+            } else {
+                return $arg1;
+            }
+        }, 6);
+        $this->tokenFactory->addFunction('mulgt', function ($arg1, $arg2, $arg3, $arg4, $arg5, $arg6, $arg7) {
+            if($arg7 > $arg6) {
+                if($arg5 > $arg4) {
+                    return $arg3;
+                } else {
+                    return $arg2;
+                }
+            } else {
+                return $arg1;
+            }
+        }, 7);
     }
 
     /**
@@ -140,6 +206,58 @@ class MathExecutor
     }
 
     /**
+     * Add middle result to executor
+     *
+     * @param $midresult
+     * @param $value
+     */
+    public function setMidResult($midresult, $value) {
+        $this->midresults[$midresult] = $value;
+    }
+
+    /**
+     * Add middle results to executor
+     *
+     * @param array $midresults
+     * @param bool $clear
+     * @return $this
+     */
+    public function setMidResults(array $midresults, $clear = true) {
+        if ($clear) {
+            $this->removeMidResults();
+        }
+
+        foreach($midresults as $name => $value) {
+            $this->setMidResult($name, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove variable from executor
+     *
+     * @param  string       $variable
+     * @return MathExecutor
+     */
+    public function removeMidResult($variable)
+    {
+        unset ($this->midresults[$variable]);
+
+        return $this;
+    }
+
+    /**
+     * Remove all variables
+     */
+    public function removeMidResults()
+    {
+        $this->midresults = array();
+
+        return $this;
+    }
+
+    /**
      * Add operator to executor
      *
      * @param  string       $operatorClass Class of operator token
@@ -184,7 +302,7 @@ class MathExecutor
             $tokens = $this->cache[$expression];
         }
         $calculator = new Calculator();
-        $result = $calculator->calculate($tokens, $this->variables);
+        $result = $calculator->calculate($tokens, $this->variables, $this->midresults, $this);
 
         return $result;
     }
