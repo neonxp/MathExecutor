9205d7c81e91838066924f6b0658ec5a61b80ed5
alfaalex81 <37154625+alfaalex81@users.noreply.github.com>
2021-05-13 12:59:02 +0200

    Update CustomFunction.php

--- src/NXP/Classes/CustomFunction.php
+++ src/NXP/Classes/CustomFunction.php
@@ -23,6 +23,7 @@ class CustomFunction
      * @var int
      */
     public $places;
+    public $placesOptional;
 
     /**
      * CustomFunction constructor.
@@ -32,28 +33,29 @@ class CustomFunction
      * @throws ReflectionException
      * @throws IncorrectNumberOfFunctionParametersException
      */
-    public function __construct(string $name, callable $function, ?int $places = null)
-    {
+    public function __construct(string $name, callable $function, ?int $places = null) {
         $this->name = $name;
         $this->function = $function;
         if ($places === null) {
             $reflection = new ReflectionFunction($function);
-            $this->places = $reflection->getNumberOfParameters();
+            $this->places = $reflection->getNumberOfRequiredParameters();
+            $this->placesOptional = $reflection->getNumberOfParameters();
         } else {
             $this->places = $places;
+            $this->placesOptional = $places;
         }
     }
 
-    public function execute(array &$stack) : Token
-    {
+    public function execute(array &$stack): Token {
         if (count($stack) < $this->places) {
             throw new IncorrectNumberOfFunctionParametersException($this->name);
         }
         $args = [];
-        for ($i = 0; $i < $this->places; $i++) {
+
+        for ($i = 0; $i < $this->placesOptional; $i++) {
             array_unshift($args, array_pop($stack)->value);
         }
-
+        $args = array_values(array_filter($args));
         $result = call_user_func_array($this->function, $args);
 
         return new Token(Token::Literal, $result);
